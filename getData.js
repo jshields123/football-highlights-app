8db4632d77d6a1256fbe61ac68c76f14b16ccf1d
jshields123 <jackshields@live.co.uk>
2021-04-27 19:41:36 +0100

    added in start of search function currently hardcoded to filter for newcastle united

--- getData.js
+++ getData.js
@@ -1,8 +1,19 @@
 const api = 'https://www.scorebat.com/video-api/v1/'
-        
+
         function getSportData(){
            return fetch(api).then(response => {
                const json = response.json()
+               
                return json
            })
         }
+
+        // below function currently filters json for Newcastle United games - need to include a search function for any club.
+        function findTeams () {
+             getSportData().then(data => {
+            const newArray = data.filter(element => element.title.includes("Newcastle United") || element.title.includes("Newcastle United"))
+            console.log(newArray[0]);
+        })
+    }
+
+       
--- renderPost.js
+++ renderPost.js
@@ -1,10 +1,14 @@
+
+
 function renderPost(sportData){
     let postTitleHTML = `<p>${sportData[randTeam]['title']}</p>`
-    let postVideoHTML = `<p>${sportData[randTeam]['videos'][0]['embed']}</p>`
-    return `${postTitleHTML}${postVideoHTML}`
+    let postDateHTML = `<p>${sportData[randTeam]['date']}</p>`
+    let postVideoHTML = `<div>${sportData[randTeam]['videos'][0]['embed']}</div>`
+    return `${postTitleHTML}${postDateHTML.slice(0,13)}${postVideoHTML}`
 }
 
 getSportData().then(post => {
 let rendered = renderPost(post)
 document.getElementById("main").innerHTML = rendered
 })
+
